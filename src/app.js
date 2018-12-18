ef35b599072c550a0958905d4d25fde382416a08
mhmdtshref <mhmdtshref@gmail.com>
2018-12-18 14:14:39 +0200

    add post request handler to add new recipe to DB #10

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');// 1
-const recipesData = require('../data.json');
+//const recipesData = require('../data.json');
+const setData = require('./queries/set');
 
 const app = express();
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.set('views', path.join(__dirname, 'views'));// 2
 app.set('view engine', 'hbs');// 3
@@ -20,7 +22,22 @@ app.engine(
 app.get('/', (req, res) => {
   res.render('home', {
     title: 'Recipes',
-    recipes: recipesData,
+    recipes: [{ name: 'name1', image_url: 'hhh', recipe: 'resipe1' }],
   });
 });
+
+
+app.post('/recipe/add', (req, res) => {
+   const {name,recipe,img_url,type}= req.body;
+  const recipeObj = { name, recipe, img_url, type };
+  setData.addRecipe(recipeObj, (err, id) => {
+    if (err) {
+      console.log('SET DATA ERROR : ', err);
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
+
 module.exports = app;
