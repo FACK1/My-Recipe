218c4f71da1f196abe3d32d8834a6c2df91818c6
samaamro20 <samaamro20@gmail.com>
2018-12-18 11:33:04 +0200

    create server to serve home.hbs with app with eslint

--- src/app.js
+++ src/app.js
@@ -1,25 +1,26 @@
-const path = require('path')
+const path = require('path');
 const express = require('express');
+const exphbs = require('express-handlebars');// 1
 const recipesData = require('../data.json');
-const exphbs = require('express-handlebars');//1
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
-app.set('views',path.join(__dirname,'views'));//2
-app.set('view engine','hbs');//3
-//4.
+app.set('views', path.join(__dirname, 'views'));// 2
+app.set('view engine', 'hbs');// 3
+// 4.
 app.engine(
   'hbs',
   exphbs({
-    extname:'hbs',
-    layoutsDir:path.join(__dirname,'views','layouts'),
-    defaultLayout:'main'
-  })
-)
+    extname: 'hbs',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    defaultLayout: 'main',
+  }),
+);
 app.get('/', (req, res) => {
-  res.render("home", {
+  res.render('home', {
     title: 'Recipes',
-    recipes:recipesData,
-  })
-})
-module.exports = app
+    recipes: recipesData,
+  });
+});
+module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
-const app = require('./app.js')
+const app = require('./app.js');
 
-const port = 3000
+const port = 3000;
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Our app is running on http://localhost:${port}`);
 });
