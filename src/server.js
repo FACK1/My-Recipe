7b5053c6bd94d4be3e61b7431794648e4c9c528c
mhmdtshref <mhmdtshref@gmail.com>
2018-12-19 15:36:59 +0200

    solve OR sign for the PORT #33

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require('./app.js');
 require('env2')('config.env');
 
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Our app is running on http://localhost:${PORT}`);
